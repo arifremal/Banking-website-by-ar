4ae31963a7d95d1be133389ae0a49725254832ec
arifremal <arifremal013@gmail.com>
2023-02-13 12:38:27 +0600

    almost done

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -1,25 +1,59 @@
-document.getElementById('btn-deposit').addEventListener('click',function(){
-    const depositField = document.getElementById('depo-field');
-    const depositFieldString = depositField.value;
-    const depositFieldFinall = parseFloat(depositFieldString);
-    depositField.value = '';
+// document.getElementById("btn-deposit").addEventListener("click", function () {
+//   const depositField = document.getElementById("depo-field");
+//   const depositFieldString = depositField.value;
+//   const depositFieldFinall = parseFloat(depositFieldString);
+//   depositField.value = "";
 
-    const depoBox = document.getElementById('deposite-totall');
-    const depoBoxInputString = depoBox.innerText;
-    const depoBoxPuttingFinall = parseFloat(depoBoxInputString);
+//   const depoBox = document.getElementById("deposite-totall");
+//   const depoBoxInputString = depoBox.innerText;
+//   const depoBoxPuttingFinall = parseFloat(depoBoxInputString);
+
+//   const balanceBox = document.getElementById("balance-totall");
+//   const balanceBoxFieldString = balanceBox.innerText;
+//   const balanceTotallAmount = parseFloat(balanceBoxFieldString);
+
+//   const depositTotallAmount = depoBoxPuttingFinall + depositFieldFinall;
+//   const balanceTotall = depositTotallAmount + balanceTotallAmount;
+//   balanceBox.innerText = balanceTotall;
+
+//   depoBox.innerText = depositTotallAmount;
+// });
+
+// mini version -------------->
+function getInputId(inputFieldId){
+    const inputField = document.getElementById(inputFieldId)
+    const inputFiledString = inputField.value;
+    const inpuFieldFinll = parseFloat(inputFiledString);
+    inputField.value = '';
+    return inpuFieldFinll
+    
+}
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId)
+    const elementString = element.innerText
+    const elementAmount = parseFloat(elementString)
+    return elementAmount
+}
+
+function setTextElementIdByValue(elementId,newValue){
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
 
 
-    const balanceBox = document.getElementById('balance-totall');
-    const balanceBoxFieldString = balanceBox.innerText;
-    const balanceTotallAmount = parseFloat(balanceBoxFieldString);
+document.getElementById("btn-deposit").addEventListener("click", function () {
 
-    const depositTotallAmount = depoBoxPuttingFinall + depositFieldFinall
-    const balanceTotall = depositTotallAmount + balanceTotallAmount
-    balanceBox.innerText = balanceTotall;
+    const newDepositAmount = getInputId ('depo-field');
+    const previousTotall = getElementValueById('deposite-totall')
+    const newdepoTotall = newDepositAmount + previousTotall;
 
-    depoBox.innerText= depositTotallAmount;
+    setTextElementIdByValue ('deposite-totall',newdepoTotall)
 
-   
+    const previousBalanceTotall = getElementValueById('balance-totall')
+    const newBalanceTotall = previousBalanceTotall + newdepoTotall;
+    setTextElementIdByValue ('balance-totall',newBalanceTotall)
 
 
-})
+
+
+  });
--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,3 +1,26 @@
+// document.getElementById('btn-withdraw').addEventListener('click', function(){
+//     const withdrawFiled = document.getElementById('withdraw-field');
+//     const withdrawFiledString = withdrawFiled.value;
+//     const withdrawFiledTotall = parseFloat(withdrawFiledString);
+//     withdrawFiled.value = '';
+//     const withdrawBox = document.getElementById('withdraw-totall');
+//     const withdrawBoxString = withdrawBox.innerText;
+//     const withdrawBoxTotall = parseFloat(withdrawBoxString);
+
+//     const withdrewAmountTotall =  withdrawBoxTotall + withdrawFiledTotall;
+
+//     const balanceBox = document.getElementById('balance-totall');
+//     const balanceBoxFieldString = balanceBox.innerText;
+//     const balanceTotallAmount = parseFloat(balanceBoxFieldString);
+//     const balancewithdraw = balanceTotallAmount - withdrewAmountTotall;
+//     balanceBox.innerText = balancewithdraw;
+
+//     withdrawBox.innerText = withdrewAmountTotall
+
+// })
+
+
+// MINI VERSION ---->
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawFiled = document.getElementById('withdraw-field');
     const withdrawFiledString = withdrawFiled.value;
